a96bcaf61047cd88ae3b03078e9fef691d7da1ec
Jad Dina <jad.dina19@gmail.com>
2026-03-17 20:15:00 -0600

    fixed npm start fail

---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+        
+        <header>
+            <img src="/images/logo.webp" alt="Sweet Scoop logo" />
+            <h2>Sweet Scoop Ice Cream</h2>
+            <div className="navbar">
+            
+            </div>
+        </header>
+        
+    )
+}
